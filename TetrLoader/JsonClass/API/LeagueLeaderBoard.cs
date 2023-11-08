9b2179589428cd5df39cc0d834ea9e468f20ce06
CSDotNET <main@csdotnet.dev>
2023-11-08 10:37:51 +0900

    added json classes for api

---
+++ TetrLoader/JsonClass/API/LeagueLeaderBoard.cs
@@ -0,0 +1,27 @@
+﻿namespace TetrLoader.JsonClass.API;
+
+public class LeagueLeaderBoard
+{
+	public bool? success { get; set; }
+	public LeagueLeaderBoardData data { get; set; }
+}
+
+public class LeagueLeaderBoardData
+{
+	public List<User> users { get; set; }
+}
+
+public class League
+{
+	public int? gamesplayed { get; set; }
+	public int? gameswon { get; set; }
+	public double? rating { get; set; }
+	public double? glicko { get; set; }
+	public double? rd { get; set; }
+	public string rank { get; set; }
+	public string bestrank { get; set; }
+	public double? apm { get; set; }
+	public double? pps { get; set; }
+	public double? vs { get; set; }
+	public bool? decaying { get; set; }
+}
---
+++ TetrLoader/JsonClass/API/LeagueStream.cs
@@ -0,0 +1,13 @@
+﻿namespace TetrLoader.JsonClass.API;
+
+public class LeagueStream
+{
+	public bool? success { get; set; }
+	public RecordData? data { get; set; }
+}
+
+
+public class RecordData
+{
+	public List<ReplayDataTTRM>? records { get; set; }
+}
